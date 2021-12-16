80f50420909d06769d542f98206c4ea99b118174
Hagenimana Yassin <hyassin509@gmail.com>
2021-12-16 17:02:31 +0200

    devops examination

--- src/test/java/rw/ac/rca/termOneExam/controller/CityControllerIntegrationTest.java
+++ src/test/java/rw/ac/rca/termOneExam/controller/CityControllerIntegrationTest.java
@@ -1,5 +1,78 @@
 package rw.ac.rca.termOneExam.controller;
 
-public class CityControllerIntegrationTest {
+import org.json.JSONException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import rw.ac.rca.termOneExam.domain.City;
+import rw.ac.rca.termOneExam.utils.APICustomResponse;
+
+import static org.junit.Assert.*;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(CityController.class)
+public class CityControllerIntegrationTest {
+  @Autowired
+  private TestRestTemplate restTemplate;
+
+
+
+  @Test
+  public void getAll_cities_success() throws JSONException {
+    String response = this.restTemplate.getForObject("api/cities/all",String.class);
+
+    JSONAssert.assertEquals("[{id:101},{id:102},{id:103},{id:104}]",response,false);
+
+  }
+
+  @Test
+  public void getById_success() throws JSONException {
+    ResponseEntity<City> city = this.restTemplate.getForEntity("api/cities/id/101",City.class);
+
+    assertTrue(city.getStatusCode().is2xxSuccessful());
+    assertEquals("Kigali",city.getBody().getName());
+    assertEquals(24,city.getBody().getWeather());
+
+  }
+
+  @Test
+  public void getById_fail() throws JSONException {
+    ResponseEntity<APICustomResponse> response = this.restTemplate.getForEntity("api/cities/id/20",APICustomResponse.class);
+
+    assertTrue(response.getStatusCodeValue()==404);
+    assertEquals(false, response.getBody().isStatus());
+    assertEquals("City not found with id 20",response.getBody().getMessage());
+
+  }
+
+  @Test
+  public void post_city_success() throws JSONException {
+    City body = new City(105,"Muhanga",70,12);
+    ResponseEntity<City> item = this.restTemplate.postForEntity("api/cities/add",body, City.class);
+
+    assertTrue(item.getStatusCode().is2xxSuccessful());
+    assertEquals("Cairo",item.getBody().getName());
+
+  }
+
+  @Test
+  public void post_City_failure() throws JSONException {
+    City body = new City(101,"Rubavu",70,12);
+    ResponseEntity<APICustomResponse> response = this.restTemplate.postForEntity("api/cities/add",body,APICustomResponse.class);
+
+    assertTrue(response.getStatusCodeValue()==400);
+    assertFalse(response.getBody().isStatus());
+    assertEquals("City name Rubavu is registered already",response.getBody().getMessage());
+
+  }
 }
--- src/test/java/rw/ac/rca/termOneExam/service/CityServiceTest.java
+++ src/test/java/rw/ac/rca/termOneExam/service/CityServiceTest.java
@@ -1,5 +1,62 @@
 package rw.ac.rca.termOneExam.service;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import rw.ac.rca.termOneExam.domain.City;
+import rw.ac.rca.termOneExam.dto.CreateCityDTO;
+import rw.ac.rca.termOneExam.repository.ICityRepository;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class CityServiceTest {
+
+  @Mock
+  private ICityRepository cityRepositoryMock;
+
+  @InjectMocks
+  private CityService cityService;
+
+  @Test
+  public void get_cities_success() {
+
+    when(cityRepositoryMock.findAll()).thenReturn(Arrays.asList(new City(104, "Kigali", 24, 100),
+        new City(2, "Gasabo", 26, 10)));
+    assertEquals(75.2, cityService.getAll().get(0).getFahrenheit());
+  }
+
+  @Test
+  public void get_city_ById_success() {
+    Long id = 104L;
+    City city = new City(id, "Musanze", 24, 80);
+    when(cityRepositoryMock.findById(id)).thenReturn(Optional.of(city));
+    assertEquals(75.2, cityService.getById(id).get());
+  }
+
+  @Test
+  public void get_city_ById_fail() {
+    when(cityRepositoryMock.findById(1000L)).thenReturn(Optional.empty());
+    City city = cityService.getById(10);
+    assertTrue(city == null);
+  }
+
+  @Test
+  public void Post_city_success() {
+    CreateCityDTO dto = new CreateCityDTO();
+    dto.setName("Nyanza");
+    dto.setWeather(24);
+    City city = new City(dto.getName(), dto.getWeather());
+    when(cityRepositoryMock.save(city)).thenReturn(city);
+    City createCity = cityService.save(dto);
+    assertTrue(createCity.getName() == "Nyanza");
+  }
 
 }
